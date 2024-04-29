a4b92fa80c5a7270104273733673af1ca39bf7d4
Nahomi Humerez <146476879+TrynaFindMimi@users.noreply.github.com>
2024-04-28 20:34:08 -0400

    Update EstrellasGrafro.cpp

--- Grafos/EstrellasGrafro.cpp
+++ Grafos/EstrellasGrafro.cpp
@@ -1,1 +1,40 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() { 
+    int filas, columnas; 
+    while (cin >> filas >> columnas && filas > 0 && columnas > 0) { 
+ 
+        char grafo[filas][columnas]; 
+ 
+        for(int i = 0; i < filas; i++) { 
+            for(int j = 0; j < columnas; j++) { 
+                cin >> grafo[i][j]; 
+            } 
+        } 
+ 
+        int estrellas = 0; 
+        for(int i = 0; i < filas; i++) { 
+            for(int j = 0; j < columnas; j++) { 
+                if (grafo[i][j] == '*') { 
+                    bool estrellaS = true; 
+                    for (int k = i-1; k <= i+1; k++) { 
+                        for (int l = j-1; l <= j+1; l++) { 
+                            if (k >= 0 && k < filas && l >= 0 && l < columnas && grafo[k][l] == '*' && (k != i || l != j)) { 
+                                estrellaS = false;  
+                            } 
+                        } 
+                    } 
+                    if (estrellaS) { 
+                        estrellas++; 
+                    } 
+                } 
+            } 
+        } 
+ 
+        cout << estrellas << endl; 
+    } 
+ 
+    return 0; 
+}
 
